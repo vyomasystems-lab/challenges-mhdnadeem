57d0a952e45547c520d5c2d11725f21c912a6d1c
mhdnadeem <mhdnadeem3579@gmail.com>
2022-07-24 17:15:05 +0000

    added test_mux

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,5 +6,13 @@ from cocotb.triggers import Timer
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
+    INP5 = 2
+    SEL= 4
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    dut.inp5.value = INP5
+    dut.sel.value = SEL
+    await Timer(2, units='ns')
+
+
+    cocotb.log.info(f'INP5={INP5} SEL={SEL} model={2} DUT={int(dut.out.value)}')
+    assert dut.out.value == 2, f'MUX result is incorrect: {dut.out.value} !=2'
